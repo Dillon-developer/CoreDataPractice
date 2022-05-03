1e23305624547fa4d7204e4f97e4eb9a8fab5c7e
Dillon Lucas <dill17lion@gmail.com>
2022-05-03 11:57:56 -0500

    Initial Commit

---
+++ Shared/CoreDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracticeApp.swift
+//  Shared
+//
+//  Created by Dillon Lucas on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
